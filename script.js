176489eda51fe51bca3fb2dd0821a2a6efb6c55b
Frozanziaee <130326629+Frozanziaee@users.noreply.github.com>
2024-02-06 00:46:40 +0430

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let homeScoreBtn = document.querySelectorAll(".btn")
+let homeStoreEl =document.getElementById("home-score")
+let guestStoreEl =document.getElementById("guest-score")
+
+    let homeScore = 0
+    let guestScore = 0
+
+    function increaseHomeScoreOne(){
+        homeScore += 1
+        homeStoreEl.textContent = homeScore
+
+    }
+
+    function increaseHomeScoreTwo(){
+        homeScore += 2
+        homeStoreEl.textContent = homeScore
+
+    }    
+
+    function increaseHomeScoreThree(){
+        homeScore += 3
+        homeStoreEl.textContent = homeScore
+
+    }
+
+    function increaseGuestScoreOne(){
+        guestScore += 1
+        guestStoreEl.textContent = guestScore
+
+    }
+
+    function increaseGuestScoreTwo(){
+        guestScore += 2
+        guestStoreEl.textContent = guestScore
+
+    }
+
+    function increaseGuestScoreThree(){
+        guestScore += 3
+        guestStoreEl.textContent = guestScore
+
+    }
+
+    function removeScore(){
+        homeScore = 0
+        guestScore = 0
+        homeStoreEl.textContent = homeScore
+        guestStoreEl.textContent = guestScore
+
+    }   
